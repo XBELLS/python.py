353dd4b4896190bd9419039306550ed41cf6c480
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-14 23:37:38 -0300

    gdbtext.py
    Add initial GDB text file with example Python code.

---
+++ gdbtext.py
@@ -0,0 +1,22 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print("Hello word")
+print(5 + 5) #sem aspas
+print(10 * 2)
+
+carname = "Volvo"
+x = 50
+print(type(carname)) #imprime o tipo
+print(carname) #imprime o nome
+
+x = z = y = "Hello World"
+
+a, b, c = "Banana", "Abacate", "Morango"
+
+#identacao tem que ter o mesmo numero de espaços no bloco
