f4ed81a8cc4260f4cf35daa9ff8e7b0068caf1e7
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 15:43:20 -0300

    indexacaoneg.py
    Add multiple print statements to display slices of the string 'Hello, World!'

---
+++ indexacaoneg.py
@@ -0,0 +1,26 @@
+b = "Hello, World!"
+print(b[-13:-1])
+b = "Hello, World!"
+print(b[-12:-1])
+b = "Hello, World!"
+print(b[-11:-1])
+b = "Hello, World!"
+print(b[-10:-1])
+b = "Hello, World!"
+print(b[-9:-1])
+b = "Hello, World!"
+print(b[-8:-1])
+b = "Hello, World!"
+print(b[-7:-1])
+b = "Hello, World!"
+print(b[-6:-1])
+b = "Hello, World!"
+print(b[-5:-1])
+b = "Hello, World!"
+print(b[-4:-1])
+b = "Hello, World!"
+print(b[-3:-1])
+b = "Hello, World!"
+print(b[-2:-1])
+b = "Hello, World!"
+print(b[-1:-1])
