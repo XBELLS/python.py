621ad86c0903a97cb77a54957ae10274ed064d49
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 16:30:58 -0300

    stringenum.py

---
+++ stringenum.py
@@ -0,0 +1,19 @@
+#combinacao de numeros e letras
+#format()
+age = 18
+txt = f"My name is isA, I am {age}" #tem q colocar o f antes do string e botar o 
+#ngc q vc quer adicionar em colchetes
+print(txt)
+
+#precificaçao
+price = 59
+Txt = f"The price is {price} dollars"
+print(Txt)
+price = 59
+
+
+Txt = f"The price is {price:.8f} dollars"
+print(Txt)
+
+Txt = f"The price is {20 * 59} dollars"
+print(Txt)
