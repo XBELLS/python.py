25867b8a16da9fe89d0344b8f043c5bb975f0b2c
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 16:04:37 -0300

    cordascortantes.py

---
+++ cordascortantesneg.py
@@ -0,0 +1,21 @@
+#slice --> cortar os bytes e limitar quantos caracteres quer pegar
+b = "Hello, World!"
+print(b[2:5]) #pega os carecteres 3, 4, 5 !nao pega o primeiro!
+
+print(b[2:]) #pega o segundo caractere ate o ultimo, nao pega o segundo
+
+b = "Hello, World! "
+print(b[-14:-1])
+print(b[-13:-1])
+print(b[-12:-1])
+print(b[-11:-1])
+print(b[-10:-1])
+print(b[-9:-1])
+print(b[-8:-1])
+print(b[-7:-1])
+print(b[-6:-1])
+print(b[-5:-1])
+print(b[-4:-1])
+print(b[-3:-1])
+print(b[-2:-1])
+print(b[-1:-1])
