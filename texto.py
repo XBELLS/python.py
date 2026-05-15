be9ddc9335ac9681db64d4bb0d9db8165f96492e
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 13:38:52 -0300

    texto.py

---
+++ texto.py
@@ -0,0 +1,32 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+"""
+x = 'awesome'
+def myfunc():
+    x = 'fantastic'
+myfunc()
+print("Python is ' + x")
+"""
+"""
+print("Hello World", end="  ")
+print("Vai printar na mesma linha por causa do end")
+
+print(3, end="  ")
+print("--> Número nao usa aspas")
+
+print(45 + 56, end="  ")
+print("--> nem contas")
+
+print("I am", 18, "years old", end=" ")
+print("-->tem q combinar numero e letra assim")
+
+"""
+"""
+x =str(3)
+y =int(3)
+z =float(3) 
+"""
+x = 5
+y = "Isa"
+
+print(type(x))
+print(type(y))
