eb8c851ae685a504a12a4da6ad145a3dfcb96e81
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 15:24:59 -0300

    intxt.py

---
+++ intxt.py
@@ -0,0 +1,25 @@
+for x in "banana":
+  print(x)
+  print(len(x)) #tamanho da corda
+
+txt = "The best things in life are free!"
+print("free" in txt) #verifica se uma palavra esta no texto
+
+txt = "The best things in life are free!"
+print("school" in txt) 
+
+Txt = "The best things in life are free!"
+if "free" in Txt:
+  print("Yes, 'free' is present.")
+  
+xt = "The best things in life are free!"
+if "school" in Txt:
+  print("Yes, 'free' is present.")
+else:
+    print("Não está")
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
