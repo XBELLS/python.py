24753ece96a9de23c8ff8927c864aee9e4d6054f
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 13:42:25 -0300

    variable.py

---
+++ variable.py
@@ -0,0 +1,13 @@
+x = "awesome"
+
+def myfunc():
+    print("Python is" + x) #n tem espaço
+    
+myfunc()
+
+X = "awesome"
+
+def myfunc():
+    print("Python is", X) #separa por espaco
+    
+myfunc()
