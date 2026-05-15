9ffd19534654d0de976e096e1b3a536f87b7b9d3
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 14:42:05 -0300

    randoandvar.py

---
+++ randoandvar.py
@@ -0,0 +1,28 @@
+import random #modulo de um numero aleatorio
+print(random.randrange(1, 50)) #sorteia um numero n vai o 1 nem o 50
+
+# Create an integer
+x = 5
+x = int(5)
+# Create a float
+y = 3.14
+y = float(3.14)
+# Create a complex number
+z = 2+3j
+z = complex(2+3j)
+# Print the types
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Create an integer
+X = 1
+# Convert to float
+a = float(X)
+# Convert to string
+b = str(X)
+# Print values
+print(X)
+print(a)
+print(b)
