2d7979d92b5e2ec0a89423547cf0541bf474075c
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 14:21:24 -0300

    tiposvar.py

---
+++ tiposvar.py
@@ -0,0 +1,28 @@
+x = ["cherry", "apple", "orange"] #chaves eh list
+print(type(x))
+
+y = ("orange", "apple", "cherry") #parenteses eh tuple
+print(type(y))
+
+z = {"name" : "isa", "age" : 18} #colchetes eh dict
+print(type(z))
+
+i = True
+print(type(i)) #true or false eh bool
+
+A = 35e3 #O E indica potencia de 10
+B = 12E4
+C = -87.7e100
+
+print(type(A))
+print(type(B))
+print(type(C))
+
+X = 3+5j #sao escritos com J como parte IMAGINARIA
+Y = 5j
+Z = -5j
+
+print(type(X))
+print(type(Y))
+print(type(Z))
+
