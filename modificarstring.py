b2fca8716a013181c710f40b0274ad90c7ef4b56
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-15 16:15:26 -0300

    modificarstring.py

---
+++ modificarstring.py
@@ -0,0 +1,10 @@
+a = " Hello, World! "
+print(a.upper()) #deixa tudo em maiuscula
+
+print(a.lower()) #deixa tudo em minuscula
+
+print(a.strip()) #remove os espaços antes e depois
+
+print(a.replace("H", "J")) #troca as letras
+
+print(a.split(",")) #vira uma lista
