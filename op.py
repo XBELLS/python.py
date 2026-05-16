732bed1a4e3a4bd29cc1263f6b9ae7d0e574ba45
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-16 15:03:21 -0300

    op.py

---
+++ op.py
@@ -0,0 +1,13 @@
+sum = 140 + 60
+sum1 = sum + 140
+sum2 = sum1 + sum1
+
+print(sum)
+print(sum1)
+print(sum2)
+
+if(sum1>sum2):
+    print("a soma 02 é maior")
+else: 
+    print("a soma 01 é maior")
+    
