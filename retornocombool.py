273354b260f63ae0533829b34419fd3351ac7e32
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-16 14:47:27 -0300

    retornocombool.py

---
+++ retornocombool.py
@@ -0,0 +1,9 @@
+def myFunction() : #def cria uma funcao #myFunction nome da func
+  return 1 #retorna valor verdadeiro
+
+print(myFunction()) #printa o valor
+
+if myFunction():
+  print("o valor retornado é verdadeiro!") 
+else:
+  print("o valor retornado é falso!")
