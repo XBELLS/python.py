d78807342114037ffad8ebb51f3c59fb41136bbb
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-16 15:03:47 -0300

    aritmetica.py

---
+++ aritmetica.py
@@ -0,0 +1,14 @@
+# + soma
+# - subtracao
+# * multiplicacao
+# / divisao
+# % resto da divisao
+# ** exponencial
+x = 2
+y = 8
+print(x ** y) #mesmo q 2*2*2*2*2
+# // floor divisao divide e pega o numero inteiro mais proximo
+a = 15
+b = 4
+print(a // b) #aproximadamente
+
