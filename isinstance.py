a3c764e7ecee2978df978fdac27d07189ce25905
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-16 14:47:52 -0300

    isinstance.py

---
+++ isinstance.py
@@ -0,0 +1,7 @@
+#isinstance()
+x = 200
+print(isinstance(x, int))
+#ve se o x pertence aos inteiros e retorna f ou t
+print(isinstance(x, float))
+#ve se o x pertence ao float e retorna f ou t
+print(isinstance(x, str)) #retorna false
