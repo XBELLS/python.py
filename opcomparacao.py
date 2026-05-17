789615533558d292c56ba46985b0bd93be459b90
ISABELLY <isabellydesousamarques123@gmail.com>
2026-05-17 19:43:20 -0300

    opcomparacao.py

---
+++ opcomparacao.py
@@ -0,0 +1,18 @@
+"""
+== igual
+!= diferente
+> maior
+< menor
+>= maior ou igual
+<= menor ou igual
+
+"""
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
